50629ad6561ff5dd617565892d7b81deda70ea47
AOA19 <aakinyemi19@gmail.com>
2024-09-29 19:30:37 -0400

    Removed comments

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-// min $10 - Win add $50
-// max $100 - Win add $500
-// Starting amount: $1000
-
-/* 
-- Create variables 
-- Create a variables for the total and set it to a default number and display it
-- Randomize the 5 items for reels 
-- Set a click event listener to the min and max buttons 
-- Create a variable to hold all the images
-- Create a function that checks if all the images match, if that's the case display "Winner" result and add to total winnings
-- Add a edge case for when the user loses all their money- If user loses all money do't allow them to bet anymore
-- (EXTRA)Maybe add an option to "loan" more money ($100) when user runs out of money
-- Update the total winnings with the 
-
-- Randomize three options 
-
-*/
-
 // Declare and display default total amount
 let total = 1000;
 document.querySelector("#totalWinnings").innerText = total;
@@ -49,10 +30,6 @@ function spinWheel(win, lose) {
   itemOne = setSlot(Math.floor(Math.random() * 5), ".slotOne");
   itemTwo = setSlot(Math.floor(Math.random() * 5), ".slotTwo");
   itemThree = setSlot(Math.floor(Math.random() * 5), ".slotThree");
-
-  //   setSlot(itemOne, "slotOne");
-  //   setSlot(itemTwo, "slotTwo");
-  //   setSlot(itemThree, "slotThree");
 
   if (checkChoices() === true) {
     total += win;
